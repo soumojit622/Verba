c2efca67e60ebed9fb2bdef6daf2287c12ba4943
soumojit622 <soumojitbanerjee22@gmail.com>
2025-05-12 21:45:26 +0530

    first commit

---
+++ config/index.ts
@@ -0,0 +1,8 @@
+import type { Metadata } from "next";
+
+export const siteConfig: Metadata = {
+  title: "Verba | Master Languages with Confidence",
+  description:
+    "Verba is a modern, AI-powered language learning platform offering immersive lessons, interactive quizzes, and real-time progress tracking to help you master new languages with confidence.",
+} as const;
+
