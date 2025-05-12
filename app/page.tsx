b75a1803b2337ce1e221d0ae8013714551178a0c
soumojit622 <soumojitbanerjee22@gmail.com>
2025-05-12 21:56:16 +0530

    ✨ Add reusable button components library for consistent UI styling

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,14 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <Button>Hello Duolingo</Button>
+    <>
+      <Button variant={"primary"}>Hello Duolingo</Button>
+      <Button variant={"secondary"}>Hello Duolingo</Button>
+      <Button variant={"danger"}>Hello Duolingo</Button>
+      <Button variant={"super"}>Hello Duolingo</Button>
+      <Button variant={"superOutline"}>Hello Duolingo</Button>
+      <Button variant={"locked"}>Hello Duolingo</Button>
+      <Button variant={"ghost"}>Hello Duolingo</Button>
+    </>
   );
 }
